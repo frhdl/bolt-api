23ac03fb95c0c502d4c5decc020f9df80ab0db13
Francisco Diniz <chipman@tutanota.com>
2021-03-21 00:22:25 -0300

    feat: add user handler.

---
+++ app/handlers/user.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/getchipman/bolt-api/app/context"
+	"github.com/getchipman/bolt-api/app/core/domains"
+	"github.com/gin-gonic/gin"
+)
+
+// Create Add a new user.
+func (hdl *HTTPHandler) Create(ctx *context.Context, c *gin.Context) error {
+
+	params := &domains.User{}
+	err := c.ShouldBindJSON(params)
+	if err != nil {
+		ctx.ResultError(1, "Invalid Request").JSON(c, nil)
+	}
+
+	result := hdl.userService.Create(ctx, params)
+
+	result.JSON(c, nil)
+	return nil
+}
