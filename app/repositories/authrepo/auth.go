b37e41698db9d19d399aa08dfe19a8e8c638a040
Francisco Diniz <chipman@tutanota.com>
2021-03-21 00:14:39 -0300

    feat: add auth and user repositories.

---
+++ app/repositories/authrepo/auth.go
@@ -0,0 +1,49 @@
+package authrepo
+
+import (
+	"fmt"
+
+	"github.com/getchipman/bolt-api/app/context"
+	"github.com/getchipman/bolt-api/app/core/domains"
+	"github.com/getchipman/bolt-api/app/core/ports"
+)
+
+// AuthRepository contains objects for database communication.
+type AuthRepository struct {
+	Db    ports.Persistence
+	Cache ports.Cache
+}
+
+// New create auth repository.
+func New(db ports.Persistence, cache ports.Cache) *AuthRepository {
+	return &AuthRepository{
+		Db:    db,
+		Cache: cache,
+	}
+}
+
+// Login return user name, user email and user id.
+func (r *AuthRepository) Login(ctx *context.Context, user *domains.User) (context.Result, int, string, string) {
+	query := fmt.Sprintf(`SELECT id, name, email from users WHERE client_id = '%v' and client_secret = '%v'`, user.ClientID, user.ClientSecret)
+
+	rows, err := r.Db.QueryRow(query)
+	if err != nil {
+		ctx.Logger.WithField("Error", err.Error()).Errorf("Error to get user - ClientID: %v", user.ClientID)
+	}
+
+	defer rows.Close()
+	var id int
+	var name string
+	var email string
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name, &email)
+		if err != nil {
+			ctx.Logger.WithField("Error", err.Error()).Errorf("Error to get user - ClientID: %v", user.ClientID)
+
+			return ctx.ResultError(2, err.Error()), 0, "", ""
+		}
+	}
+
+	return ctx.ResultSuccess(), id, name, email
+}
---
+++ app/repositories/usersrepo/user.go
@@ -0,0 +1,62 @@
+package usersrepo
+
+import (
+	"fmt"
+
+	"github.com/getchipman/bolt-api/app/context"
+	"github.com/getchipman/bolt-api/app/core/domains"
+	"github.com/getchipman/bolt-api/app/core/ports"
+)
+
+// UserRepository contains objects for database communication.
+type UserRepository struct {
+	Db    ports.Persistence
+	Cache ports.Cache
+}
+
+//New create a User repository.
+func New(db ports.Persistence, cache ports.Cache) *UserRepository {
+	return &UserRepository{
+		Db:    db,
+		Cache: cache,
+	}
+}
+
+// Create save a new user.
+func (r *UserRepository) Create(ctx *context.Context, user *domains.User) context.Result {
+	query := fmt.Sprintf(`
+		INSERT INTO users(name, email, client_id, client_secret, create_at, update_at)
+		VALUES ('%v', '%v', '%v', '%v', NOW(), NOW())`, user.Name, user.Email, user.ClientID, user.ClientSecret)
+
+	_, err := r.Db.QueryRow(query)
+	if err != nil {
+		ctx.Logger.WithField("Error", err.Error()).Errorf("Error to insert user - Query: %v", query)
+		return ctx.ResultError(1, err.Error())
+	}
+
+	return ctx.ResultSuccess()
+}
+
+// FindUser find a user in database.
+func (r *UserRepository) Find(ctx *context.Context, user *domains.User) (context.Result, string, string) {
+	query := fmt.Sprintf(`SELECT email, client_id FROM users WHERE email = '%v' or client_id = '%v'`, user.Email, user.ClientID)
+	rows, err := r.Db.QueryRow(query)
+	if err != nil {
+		ctx.Logger.WithField("Error", err.Error()).Errorf("Error to get user - Email: %v, ClientID: %v", user.Email, user.ClientID)
+		return ctx.ResultError(1, err.Error()), "", ""
+	}
+
+	defer rows.Close()
+	var email string
+	var clientID string
+
+	for rows.Next() {
+		err := rows.Scan(&email, &clientID)
+		if err != nil {
+			ctx.Logger.WithField("Error", err.Error()).Errorf("Error to get user - Email: %v, ClientID: %v", user.Email, user.ClientID)
+			return ctx.ResultError(2, err.Error()), "", ""
+		}
+	}
+
+	return ctx.ResultSuccess(), email, clientID
+}
