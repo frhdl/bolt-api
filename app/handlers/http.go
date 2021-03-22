7ff2ff9726170880c60a35d24eee38e7d4813589
Francisco Diniz <chipman@tutanota.com>
2021-03-22 03:42:36 -0300

    feat: update handler.

--- app/handlers/http.go
+++ app/handlers/http.go
@@ -4,14 +4,22 @@ import "github.com/getchipman/bolt-api/app/core/ports"
 
 // HTTPHandler represents a default handler.
 type HTTPHandler struct {
-	authService ports.AuthService
-	userService ports.UserService
+	authService    ports.AuthService
+	userService    ports.UserService
+	projectService ports.ProjectService
+	taskService    ports.TaskService
 }
 
 // New create a new instance of HTTPHandler.
-func New(authService ports.AuthService, userService ports.UserService) *HTTPHandler {
+func New(
+	authService ports.AuthService,
+	userService ports.UserService,
+	projectService ports.ProjectService,
+	taskService ports.TaskService) *HTTPHandler {
 	return &HTTPHandler{
-		authService: authService,
-		userService: userService,
+		authService:    authService,
+		userService:    userService,
+		projectService: projectService,
+		taskService:    taskService,
 	}
 }
