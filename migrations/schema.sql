8dbfdf7d54026c8b06cba5b003dc4c01eb4428f5
Francisco Diniz <chipman@tutanota.com>
2021-03-22 03:43:53 -0300

    feat: update migrations. Add project and task tables.

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -8,3 +8,24 @@ CREATE TABLE IF NOT EXISTS Users (
     update_at TIMESTAMP,
     CONSTRAINT user_unique UNIQUE (client_id, email)
 );
+
+CREATE TABLE IF NOT EXISTS Projects (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    user_id BIGINT,
+    create_at TIMESTAMP,
+    CONSTRAINT project_unique UNIQUE(name, user_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Tasks (
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    user_id BIGINT,
+    project_id BIGINT,
+    create_at TIMESTAMP,
+    finish_at TIMESTAMP,
+    done boolean,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES Projects(id) ON DELETE CASCADE
+);
