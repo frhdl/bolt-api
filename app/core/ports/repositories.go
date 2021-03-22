9a96f198f1222bb26551d39597c6e36ea5ecde6c
Francisco Diniz <chipman@tutanota.com>
2021-03-22 03:40:32 -0300

    feat: update ports.

--- app/core/ports/repositories.go
+++ app/core/ports/repositories.go
@@ -15,3 +15,20 @@ type UserRepository interface {
 	Create(*context.Context, *domains.User) context.Result
 	Find(ctx *context.Context, user *domains.User) (context.Result, string, string)
 }
+
+// ProjectRepository interface for project repository.
+type ProjectRepository interface {
+	Create(ctx *context.Context, project *domains.Project) context.Result
+	GetAll(ctx *context.Context, page int, limit int) (context.Result, []*domains.Project)
+	Update(ctx *context.Context, project *domains.Project) context.Result
+	Delete(ctx *context.Context, projectID int) context.Result
+	Find(ctx *context.Context, project *domains.Project) (context.Result, string)
+}
+
+// TaskRepository interface for task repository.
+type TaskRepository interface {
+	Create(ctx *context.Context, task *domains.Task) context.Result
+	GetAll(ctx *context.Context, projectID int, page int, limit int) (context.Result, []*domains.Task)
+	Update(ctx *context.Context, task *domains.Task) context.Result
+	Delete(ctx *context.Context, taskID int) context.Result
+}
--- app/core/ports/services.go
+++ app/core/ports/services.go
@@ -15,3 +15,20 @@ type UserService interface {
 	Create(*context.Context, *domains.User) context.Result
 	Find(ctx *context.Context, user *domains.User) (context.Result, string, string)
 }
+
+// ProjectRepository interface for project service.
+type ProjectService interface {
+	Create(ctx *context.Context, project *domains.Project) context.Result
+	GetAll(ctx *context.Context, page int, limit int) (context.Result, []*domains.Project)
+	Update(ctx *context.Context, project *domains.Project) context.Result
+	Delete(ctx *context.Context, projectID int) context.Result
+	Find(ctx *context.Context, project *domains.Project) (context.Result, string)
+}
+
+// TaskRepository interface for task service.
+type TaskService interface {
+	Create(ctx *context.Context, task *domains.Task) context.Result
+	GetAll(ctx *context.Context, projectID int, page int, limit int) (context.Result, []*domains.Task)
+	Update(ctx *context.Context, task *domains.Task) context.Result
+	Delete(ctx *context.Context, taskID int) context.Result
+}
